11f5d0ce08c4fbc8c9d2ba7dd7a0b6084d8f1183
107000175 <nlx1451@gmail.com>
2021-01-07 23:41:18 +0800

    01/07 last commit

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ form.append("password", "123456789");
 var Today = new Date();
 var curDate = Today.getFullYear() +"-"+ (Today.getMonth()+1) +"-"+ Today.getDate();
 var curTime = Today.getHours() + ":" + Today.getMinutes() + ":" + Today.getSeconds();
-var data_appear = false;
+var sensor1_getData = false;
+var sensor2_getData = false;
 
 var cheat_data = [{
     "id": 6950426,
@@ -25,6 +26,43 @@ var cheat_data = [{
     "snr": -17   
 }];
 
+var cheat_data2 = [{
+    "id": 7286099,
+    "macaddr": "aac2949f",
+    "data": null,
+    "lat": null,
+    "lng": null,
+    "created_at": "2021-01-07 17:53:00",
+    "updated_at": "2021-01-07 17:53:00",
+    "temperature": null,
+    "humidity": null,
+    "barometer": null,
+    "acc_x": "0.031",
+    "acc_y": "0",
+    "acc_z": "-0.062",
+    "frame_cnt": 868,
+    "snr": 1
+}];
+
+function result_control(){
+    if(sensor1_getData == true){
+        if(sensor2_getData == true){// both are true
+            document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
+            
+        }
+        else{ //sensor1 is true
+            
+        }
+    }
+    else {
+        if(sensor2_getData == true){ //sensor2 is true
+
+        }
+        else{ //both are false
+
+        }
+    }
+}
 function main_control(data,curDate,curTime){
     console.log(data);
     document.getElementById("TIME").innerHTML = "資料更新時間: " + curDate + " " + curTime;
@@ -33,45 +71,77 @@ function main_control(data,curDate,curTime){
         console.log(len);
         console.log(data[0].acc_x);
         if(data[0]['acc_x'] != null || data[0]['acc_y'] != null || data[0]['acc_z'] != null){
-            data_appear = true;
+            sensor1_getData = true;
         }
-        console.log("data_appear: " + data_appear);
+        console.log("sensor1_getData " + sensor1_getData);
 
-        if(data_appear != false){
-            document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
-            document.getElementById("acc_x").innerHTML = data[0]['acc_x'];
-            document.getElementById("acc_y").innerHTML = data[0]['acc_y'];
-            document.getElementById("acc_z").innerHTML = data[0]['acc_z'];
+        if(sensor1_getData != false){
+            //document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
         }
         else{
-            document.getElementById("wms").innerHTML = "<img src=\"idle.gif\" width = 45%/>";
-            document.getElementById("acc_x").innerHTML = "NULL";
-            document.getElementById("acc_y").innerHTML = "NULL";
-            document.getElementById("acc_z").innerHTML = "NULL";
+            if(sensor1_getData == true){
+                sensor1_getData = false;
+                //document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
+            }
+            else{
+                //document.getElementById("wms").innerHTML = "<img src=\"idle.gif\" width = 45%/>";
+            }
         }
     } 
     else {
-        if(data_appear == true){
-            data_appear = false;
+        if(sensor1_getData == true){
+            sensor1_getData = false;
+            //document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
+        }
+        else{
+            //document.getElementById("wms").innerHTML = "<img src=\"idle.gif\" width = 45%/>";
+        }
+    }
+}
+
+function main_control2(data,curDate,curTime){
+    console.log(data);
+    document.getElementById("TIME").innerHTML = "資料更新時間: " + curDate + " " + curTime;
+    if(data.length != 0){
+        var len = data.length;
+        console.log(len);
+        console.log(data[0].acc_x);
+        if(data[0]['acc_x'] != null || data[0]['acc_y'] != null || data[0]['acc_z'] != null){
+            sensor2_getData = true;
+        }
+        console.log("sensor1_getData " + sensor2_getData);
+
+        if(sensor2_getData != false){
             document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
-            document.getElementById("acc_x").innerHTML = data[0]['acc_x'];
-            document.getElementById("acc_y").innerHTML = data[0]['acc_y'];
-            document.getElementById("acc_z").innerHTML = data[0]['acc_z'];
+        }
+        else{
+            if(sensor2_getData == true){
+                sensor2_getData = false;
+                document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
+            }
+            else{
+                document.getElementById("wms").innerHTML = "<img src=\"idle.gif\" width = 45%/>";
+            }
+        }
+    } 
+    else {
+        if(sensor2_getData == true){
+            sensor2_getData = false;
+            document.getElementById("wms").innerHTML = "<img src=\"working.gif\" width = 45%/>";
         }
         else{
             document.getElementById("wms").innerHTML = "<img src=\"idle.gif\" width = 45%/>";
-            document.getElementById("acc_x").innerHTML = "NULL";
-            document.getElementById("acc_y").innerHTML = "NULL";
-            document.getElementById("acc_z").innerHTML = "NULL";
         }
     }
 }
 
 main_control(cheat_data,curDate,curTime);
+main_control2(cheat_data2,curDate,curTime);
 
 $(document).ready(function getAPI() {
     function getAPI(){
         var macaddr = "aaa9f5f5";
+        var macaddr2 = "aac2949f";
         Today = new Date();
         curDate = Today.getFullYear() +"-"+ (Today.getMonth()+1) +"-"+ Today.getDate();
         curTime = Today.getHours() + ":" + Today.getMinutes() + ":" + Today.getSeconds();
@@ -95,6 +165,20 @@ $(document).ready(function getAPI() {
             "contentType": false,
             "data": form
         };
+
+        var settings2 = {
+            "url": "https://cors-anywhere.herokuapp.com/"+ "https://campus.kits.tw/api/get/data/" + macaddr2 + date_filter,
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+              "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjhkNjBkYjVkYzEwMWVjNjFiYmQ1OWZlM2U4Y2IzNzRhMjVkZjk0ZTFkZDczOTNhNTM1NzI5NDhjYzA4MGJiMWZhNWYwZWE0NzI5NTc1ZTE1In0.eyJhdWQiOiIyIiwianRpIjoiOGQ2MGRiNWRjMTAxZWM2MWJiZDU5ZmUzZThjYjM3NGEyNWRmOTRlMWRkNzM5M2E1MzU3Mjk0OGNjMDgwYmIxZmE1ZjBlYTQ3Mjk1NzVlMTUiLCJpYXQiOjE2MDkzMzQ3NDcsIm5iZiI6MTYwOTMzNDc0NywiZXhwIjoxNjQwODcwNzQ3LCJzdWIiOiI0OTYiLCJzY29wZXMiOltdfQ.OBIpI_JiYH0IrzhgtSrAbHA-fzCXmZ7bZhN4ky-MjqlcWSaaKoIKhGgVrapADyX2Q7wsIuOndaOeohWqQNbycNSvrtGYFPWL0qvYgGkVbf0V3KkuhaTHSxXji1sgfL1sUN9EnXZnG0pLJHLBAbcJ9ZspXR8o2gRawJCv0aVvY9TDWePs9fQztn7zI0FH4epuKutyukWneZzO3TSsgSD_G-zLLb5iLTlUHPyodxWE8VXAs4Bq2wduDCV1G8oT2TvZQ5i2n-9tNHO-0GLnYnfK3GXrciyHyoReco5ugSudQLOScD9rVZOARvod3vkvKD65_3kXmGpObcEwqKunyEFe_yh80xR8NLYvVOygmqmwOaHVqYV4MgZZD4cLceC8N_ci24UoO4HUcPeo-cmoVZzi5XJ3AfXO8eBpWakfxPwssRRuvoA9rQAFAO7WKnULZRmoVDd_XIk_2qGlTQ9e0WXWSFBZumQh9Hx3vVYCpP8BudDlarnNel6l2Jyz24XqzwfKHsDtWvx-M2_eu1nAGGaHnn0RBDJ_fAPxDKwOwET9klfXUE8fI8OC-t411ZDFYR1nzrObic5fmjzWm4Ycssb4vM1yh8oCfIK42KLXWYjNYAqH0G9UkWz_dtGAhZ_WVl2aP98xz3ruK2ssxlg4Iur2BlndoKRYtSrrOED6B1fI2QM",
+              "Accept": "application/json",
+            },
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
           
         $.ajax(settings).done(function (response) {
             var obj = eval(response);
@@ -102,6 +186,12 @@ $(document).ready(function getAPI() {
             main_control(obj,curDate,curTime);
         });
 
+        $.ajax(settings2).done(function (response) {
+            var obj = eval(response);
+            console.log(obj);
+            main_control2(obj,curDate,curTime);
+        });
+        result_control();
     }
     //setInterval(getAPI, 60000);
 });
